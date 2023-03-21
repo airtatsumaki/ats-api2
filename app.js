fca650e53004b0ec3cf726307aa658d2edb47da7
airtatsumaki <naz1mb20@gmail.com>
2023-03-21 14:31:14 +0000

    init database via api

--- app.js
+++ app.js
@@ -7,20 +7,42 @@ const mongoose = require('mongoose');
 const ObjectId = require('mongodb').ObjectId; 
 mongoose.set('strictQuery', true);
 
-const applicantSchema = new mongoose.Schema({
+const candidateSchema = new mongoose.Schema({
   name: {type: String},
   email: {type: String},
   blurb: {type: String},
   cvPath: {type: String}
 });
-const Applicant = mongoose.model("Applicant", applicantSchema);
+const Candidate = mongoose.model("Candidate", candidateSchema);
 
-const jobSchema = new mongoose.Schema({
+const recruiterSchema = new mongoose.Schema({
+  name: {type: String},
+  email: {type: String},
+});
+const Recruiter = mongoose.model("Recruiter", recruiterSchema);
+
+const applicationSchema = new mongoose.Schema({
+  applicant: candidateSchema,
+  appSource: recruiterSchema,
+  appProgress: {type: Number}
+  // 0 - Apply
+  // 1 - Screen
+  // 2 - 1st Interview
+  // 3 - 2nd Interview
+  // 4 - 2nd Interview with task
+  // 5 - 3rd Interview
+  // 6 - 3rd Interview with task
+  // 7 - Offer
+  // 8 - No offer
+});
+const Application = mongoose.model("Application", applicationSchema);
+
+const roleSchema = new mongoose.Schema({
   role: {type: String},
   roleDescription: {type: String},
-  jobApplicants: [applicantSchema]
+  jobApplicants: [applicationSchema]
 });
-const Job = mongoose.model("Job", jobSchema);
+const Role = mongoose.model("Role", roleSchema);
 
 connect().catch(err => console.log(err));
 
@@ -32,49 +54,59 @@ async function connect() {
 app.route("/")
   .get(async function (req, res) {
     console.log(req.body);
-    res.send({message:'Hello World new'});
+    res.send({message:'Hello World'});
   });
 
-app.route("/applicants")
+app.route("/candidates")
   .get(async function (req, res){
-    res.send(await Applicant.find());
+    res.send(await Candidate.find());
   });
 
 app.route("/applicants/:job_id")
   .get(async function (req, res){
-    var id = req.params.job_id;       
-    var o_id = new ObjectId(id);
-    console.log("applicants for role: " + o_id);
-    const result = await Job.findOne( { _id: o_id} );
-    const {jobApplicants} = result;
-    console.log(jobApplicants);
-    
-    // var myArr = [1];
-    // var returnArray = myArr.map((element) => {
-    //   return element + 1;
+    // "64198008c274e47ba4464bad"
+    const role = await Role.findOne({_id: req.params.job_id});
+    const {jobApplicants} = role;
+    // console.log(jobApplicants);
+    // const candidates = jobApplicants.map((can) => {
+    //   // const {applicant: {name}} = can;
+    //   // const {appProgress} = can;
+    //   return appProgress;
     // })
-    // console.log(returnArray)
-
-    const applicants = await Applicant.find();
-    // console.log(applicants);
-
-    var appsOfJob = applicants.filter((element) => {
-      var aid = element._id
-      console.log(jobApplicants.indexOf({_id: aid}));
-    });
-
-    // jobApplicants.forEach(async function (element) { 
-    //   const app = await Applicant.findOne( { element } );
-    //   console.log(app)
-    //   applicantObjs.push(app);
-    //  });
-    // res.send(applicantObjs);
-    res.send("done");
+    res.send(jobApplicants);
   });
 
-app.route("/jobs")
+app.route("/roles")
   .get(async function (req, res){
-    res.send(await Job.find());
+    res.send(await Role.find());
+  });
+
+app.route("/init")
+  .get(async function (req, res){
+    const rec = new Recruiter({
+      name: "Alexandra Buckley",
+      email: "ab@totaljobs.co.uk"
+    });
+    rec.save();
+
+    // const role = await Role.findOne({_id: "64198008c274e47ba4464bad"});
+    // const can1 = await Candidate.findOne({_id: "6419812c475a0215cba08b16"});
+    // let app1 = new Application({
+    //   applicant: can1,
+    //   appProgress: 0
+    // });
+    // // app1.save();
+    // role.jobApplicants.push(app1);
+    // role.save();
+
+    // var can1 = new Candidate({
+    //   name:   "Steve",
+    //   email:  "fergie@incite.ws",
+    //   blurb:  "the other architect",
+    //   cvPath: ""
+    // });
+    // can1.save();
+    res.send("complete");
   });
 
 app.listen(process.env.PORT || 8080, () => console.log("Server is running on port 8080"));
